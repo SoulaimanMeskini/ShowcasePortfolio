34a8c657c0d4fa3ffa7053420be2cdd32248a8b8
Twan L <twanluttik@gmail.com>
2024-06-13 13:09:07 -0700

    optimized images and added dist to ignore

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,14 +1,21 @@
-import React, { useRef, useLayoutEffect } from 'react';
-import { motion, useAnimation } from 'framer-motion';
-import SectionTemplate from './aboutsections/SectionTemplate';
-import SectionOne from './aboutsections/SectionOne';
-import AboutMeImage from '../assets/AboutMeImage1.jpg';
-import AboutMeImage2 from '../assets/AboutMeImage2.jpg';
-import AboutMeImage3 from '../assets/AboutMeImage3.jpg';
+import { useLayoutEffect } from "react";
+import { motion, useAnimation } from "framer-motion";
+import SectionTemplate from "./aboutsections/SectionTemplate";
+import SectionOne from "./aboutsections/SectionOne";
+import AboutMeImage from "../assets/AboutMeImage1.webp";
+import AboutMeImage2 from "../assets/AboutMeImage2.webp";
+import AboutMeImage3 from "../assets/AboutMeImage3.webp";
 import {
-  AboutTitle1, AboutTitle2, AboutText1, AboutText2, AboutImgText1, AboutImgText2,
-  AboutTitle3, AboutText3, AboutImgText3
-} from '../text/text';
+  AboutTitle1,
+  AboutTitle2,
+  AboutText1,
+  AboutText2,
+  AboutImgText1,
+  AboutImgText2,
+  AboutTitle3,
+  AboutText3,
+  AboutImgText3,
+} from "../text/text";
 
 const AboutMe = ({ setShowLogo, scrollRef }) => {
   const controls1 = useAnimation();
@@ -18,10 +25,10 @@ const AboutMe = ({ setShowLogo, scrollRef }) => {
 
   useLayoutEffect(() => {
     const handleScroll = () => {
-      const firstSection = document.getElementById('section1');
-      const secondSection = document.getElementById('section2');
-      const thirdSection = document.getElementById('section3');
-      const fourthSection = document.getElementById('section4');
+      const firstSection = document.getElementById("section1");
+      const secondSection = document.getElementById("section2");
+      const thirdSection = document.getElementById("section3");
+      const fourthSection = document.getElementById("section4");
 
       if (firstSection && secondSection && thirdSection && fourthSection) {
         const firstBounding = firstSection.getBoundingClientRect();
@@ -31,9 +38,11 @@ const AboutMe = ({ setShowLogo, scrollRef }) => {
 
         if (firstBounding.top <= 0 && firstBounding.bottom > 0) {
           setShowLogo(false);
-        } else if ((secondBounding.top <= 0 && secondBounding.bottom > 0) || 
-                   (thirdBounding.top <= 0 && thirdBounding.bottom > 0) || 
-                   (fourthBounding.top <= 0 && fourthBounding.bottom > 0)) {
+        } else if (
+          (secondBounding.top <= 0 && secondBounding.bottom > 0) ||
+          (thirdBounding.top <= 0 && thirdBounding.bottom > 0) ||
+          (fourthBounding.top <= 0 && fourthBounding.bottom > 0)
+        ) {
           setShowLogo(true);
         } else {
           setShowLogo(false);
@@ -43,13 +52,13 @@ const AboutMe = ({ setShowLogo, scrollRef }) => {
 
     const scrollContainer = scrollRef.current;
     if (scrollContainer) {
-      scrollContainer.addEventListener('scroll', handleScroll);
-      handleScroll();  // Initialize state based on initial scroll position
+      scrollContainer.addEventListener("scroll", handleScroll);
+      handleScroll(); // Initialize state based on initial scroll position
     }
 
     return () => {
       if (scrollContainer) {
-        scrollContainer.removeEventListener('scroll', handleScroll);
+        scrollContainer.removeEventListener("scroll", handleScroll);
       }
     };
   }, [setShowLogo, scrollRef]);
@@ -57,23 +66,23 @@ const AboutMe = ({ setShowLogo, scrollRef }) => {
   useLayoutEffect(() => {
     const observerOptions = {
       root: scrollRef.current,
-      threshold: 0.1
+      threshold: 0.1,
     };
 
     const observerCallback = (entries) => {
-      entries.forEach(entry => {
+      entries.forEach((entry) => {
         let control = null;
         switch (entry.target.id) {
-          case 'section1':
+          case "section1":
             control = controls1;
             break;
-          case 'section2':
+          case "section2":
             control = controls2;
             break;
-          case 'section3':
+          case "section3":
             control = controls3;
             break;
-          case 'section4':
+          case "section4":
             control = controls4;
             break;
         }
@@ -88,17 +97,23 @@ const AboutMe = ({ setShowLogo, scrollRef }) => {
       });
     };
 
-    const observer = new IntersectionObserver(observerCallback, observerOptions);
-    const sections = document.querySelectorAll('section');
-    sections.forEach(section => observer.observe(section));
+    const observer = new IntersectionObserver(
+      observerCallback,
+      observerOptions
+    );
+    const sections = document.querySelectorAll("section");
+    sections.forEach((section) => observer.observe(section));
 
     return () => {
-      sections.forEach(section => observer.unobserve(section));
+      sections.forEach((section) => observer.unobserve(section));
     };
   }, [controls1, controls2, controls3, controls4, scrollRef]);
 
   return (
-    <div className="relative h-screen w-screen md:snap-y md:snap-mandatory overflow-y-scroll no-scrollbar scroll-container" ref={scrollRef}>
+    <div
+      className="relative h-screen w-screen md:snap-y md:snap-mandatory overflow-y-scroll no-scrollbar scroll-container"
+      ref={scrollRef}
+    >
       <motion.div style={{ scaleX: 1 }} />
       <motion.section
         id="section1"
@@ -116,7 +131,13 @@ const AboutMe = ({ setShowLogo, scrollRef }) => {
         animate={controls2}
         transition={{ duration: 0.7 }}
       >
-        <SectionTemplate imgsource={AboutMeImage} imgalt="Portrait" imgtext={AboutImgText1} title={AboutTitle1} text={AboutText1} />
+        <SectionTemplate
+          imgsource={AboutMeImage}
+          imgalt="Portrait"
+          imgtext={AboutImgText1}
+          title={AboutTitle1}
+          text={AboutText1}
+        />
       </motion.section>
       <motion.section
         id="section3"
@@ -125,7 +146,14 @@ const AboutMe = ({ setShowLogo, scrollRef }) => {
         animate={controls3}
         transition={{ duration: 0.7 }}
       >
-        <SectionTemplate imgsource={AboutMeImage2} imgalt="Van Gogh" imgtext={AboutImgText2} title={AboutTitle2} text={AboutText2} cv={true} />
+        <SectionTemplate
+          imgsource={AboutMeImage2}
+          imgalt="Van Gogh"
+          imgtext={AboutImgText2}
+          title={AboutTitle2}
+          text={AboutText2}
+          cv={true}
+        />
       </motion.section>
       <motion.section
         id="section4"
@@ -134,7 +162,14 @@ const AboutMe = ({ setShowLogo, scrollRef }) => {
         animate={controls4}
         transition={{ duration: 0.7 }}
       >
-        <SectionTemplate imgsource={AboutMeImage3} imgalt="Modern Portrait" imgtext={AboutImgText3} title={AboutTitle3} text={AboutText3} socials={true} />
+        <SectionTemplate
+          imgsource={AboutMeImage3}
+          imgalt="Modern Portrait"
+          imgtext={AboutImgText3}
+          title={AboutTitle3}
+          text={AboutText3}
+          socials={true}
+        />
       </motion.section>
     </div>
   );
