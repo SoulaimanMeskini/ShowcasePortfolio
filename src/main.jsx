4f72eb6d17bc2403417e0b5ef6c0d939ee188114
Twan L <twanluttik@gmail.com>
2024-06-14 11:47:45 -0700

    added plausible analytics

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,11 @@ import "./styles/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <script
+      defer
+      data-domain="soulaimanmeskini.com"
+      src="https://plausible.io/js/script.js"
+    ></script>
     <Router />
   </React.StrictMode>
 );
