bd69c070509ff8faa07dbae29d3413870c4386c4
Twan L <twanluttik@gmail.com>
2024-06-14 11:53:55 -0700

    wrong place

--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,6 @@ import "./styles/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <script
-      defer
-      data-domain="soulaimanmeskini.com"
-      src="https://plausible.io/js/script.js"
-    ></script>
     <Router />
   </React.StrictMode>
 );
